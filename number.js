93b50905e969e7bf925aed721fc1c04a8dd13465
todyeag6@gmail.com <todyeag6@gmail.com>
2024-11-26 20:26:10 -0600

    numbInts

---
+++ number.js
@@ -0,0 +1,51 @@
+//const whole = 33;
+//const posfloat = 34.99;
+//const negfloat = -10.99;
+
+//const maxfloat = 9.12345678912345632;
+//console.log(maxfloat);
+
+//const bigIntNum = 81672567458742867654634n;
+
+//console.log(bigIntNum);
+
+//const binNum = 0b11010;
+//const hexNum = 0x1a;
+//const octNum = 0o32
+//console.log(binNum);
+//console.log(hexNum);
+//console.log(octNum);
+
+//exponential literals
+const expo1 = 0e-3
+const expo2 = 4e56
+const expo3 = 10.3e-10
+//console.log(expo1);
+//console.log(expo2);
+//console.log(expo3);billygithub
+
+//numeric separators
+const sep1 = 9_563.25
+const sep2 = 0b1011_1010
+const sep3 = 0xa0_b0_c0
+//console.log(sep1);
+//console.log(sep2);
+//console.log(sep3);
+
+//Stattic number property
+//console.log(Number.MAX_SAFE_INTEGER);
+//console.log(Number.MAX_VALUE);
+//console.log(Number.MIN_SAFE_INTEGER);
+//console.log(Number.MIN_VALUE);
+
+//static number method
+console.log(Number.isNaN(NaN));
+
+
+
+
+
+
+
+
+
