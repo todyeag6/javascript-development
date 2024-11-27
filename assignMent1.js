be5cd81214056a5919544b870e783ab8d6542aa1
todyeag6@gmail.com <todyeag6@gmail.com>
2024-11-26 20:03:29 -0600

    wk1tsk1

---
+++ assignMent1.js
@@ -0,0 +1,5 @@
+console.log("This is my first sentence.")
+console.log("I am new to JavaScript.")
+console.log("I am learning stuff everyday.")
+
+// Instructor's comments: Very good, Curtis! Keep up with the good work and keep practicing.
