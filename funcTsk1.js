c7dc930495290045ffffae68595babdcbfa24c83
todyeag6@gmail.com <todyeag6@gmail.com>
2024-11-29 17:11:13 -0600

    funcTsk1&2

---
+++ funcTsk1.js
@@ -0,0 +1,13 @@
+/*Write a function expression for adding two strings together. This expression should take 2 parameters.
+
+This function expression should return both strings using the + sign. Be sure to test the function
+
+expression using a console.log statement.*/
+
+const joinStr = function (str1, str2) {
+    let joined = (str1 + str2)
+    return joined
+}
+
+
+console.log(joinStr("Hole in", " one"));
---
+++ funcTsk2.js
@@ -0,0 +1,14 @@
+/*Write a function that will have 2 parameters. One of the parameters will have a default value of 15. This
+
+function should have a return that adds both parameters together. Be sure to test this function using 2
+
+console.log statements8?*/
+
+const addInt = function (int, int2 = 15) {
+    let upTick = int + int2
+    return upTick 
+   k
+    
+}
+console.log(addInt(30));
+console.log(addInt(45, 60));
