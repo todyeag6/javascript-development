b22e5415a065c2abc70261ed5c1692bf02814edd
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-02 12:32:13 -0600

    funcs

---
+++ funcTsk4.js
@@ -0,0 +1,6 @@
+/*Write a function that takes two parameters; first parameter is a first name, and second is a last name. 
+The function prints out a full name. Capitalize the first letter for the first and last name.*/
+
+function (first, last) {
+    
+}
---
+++ funcTsk5.js
@@ -0,0 +1,2 @@
+/*Write a function that calculates and returns an area of a circle.
+ The function should have two parameters. One is a default parameter for the pi 3.14. Fix the answer to two decimal places.*/
