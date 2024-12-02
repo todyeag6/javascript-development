87bd1171c8239964c37aaf6d7d0528b7fe9167ab
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-02 15:10:40 -0600

    dfg

---
+++ opTsks.js
@@ -0,0 +1,54 @@
+/*const addSub=(num1, num2) => {
+    const add=num1+num2;
+    const sub=num2-num1;
+    console.log(`addition ${add}, subtraction ${sub}`);
+
+}
+addSub(65, 65)*/
+
+/*multiply
+const multDiv=(num1, num2) => {
+    const mult=num1*num2;
+    const div=num2/num1;
+    console.log(`mult ${mult}, div ${div}`);
+    
+}
+multDiv(20, -15)
+
+//modulus=remainders value
+const exMod=() => {
+    const ans1=10%4
+    const ans2=12%3
+    console.log(`${ans1}, ${ans2}`);
+    
+}
+exMod()*/
+
+//exponentials
+/*const exExpo = (params) => {
+    const answer = 3**params
+    console.log(answer)
+    
+}
+exExpo(2)*/
+
+//pemdas
+//const orderOfOps=()=> (30 /(4+8)-3**5)
+//console.log(orderOfOps());
+
+//assignment operators
+//const addAssign=() => {
+  //  let numAdd1=5
+    //const numAdd2=10
+    //numAdd1+=numAdd2
+
+    //console.log(`${numAdd1}`);
+//}
+//addAssign()
+
+
+
+
+
+
+
