551479c364e1429077de0106e53f57f6b9aa9733
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-02 15:03:56 -0600

    asf

--- funcTsk4.js
+++ funcTsk4.js
@@ -2,9 +2,9 @@
 The function prints out a full name. Capitalize the first letter for the first and last name.*/
 
 const nameO = (first, last) => {
-    let name1 = first.charAt(0).toUpperCase
-    let name2 = last.charAt(0).toUpperCase
-    console.log(`${name1}, + ${name2}`);
+    let name1 = first.charAt(0).toUpperCase() + first.slice(1);
+    let name2 = last.charAt(0).toUpperCase() + last.slice(1);
+    console.log(`${name1}, ${name2}`);
     
 }
-nameO(tommy, kilmo)
+nameO("tommy", "kilmo")
