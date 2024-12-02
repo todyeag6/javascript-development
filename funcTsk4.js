a26a198382f8a127fde3c954cf38c33660a4aeed
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-02 14:56:30 -0600

    asdasdads

--- funcTsk4.js
+++ funcTsk4.js
@@ -1,6 +1,10 @@
 /*Write a function that takes two parameters; first parameter is a first name, and second is a last name. 
 The function prints out a full name. Capitalize the first letter for the first and last name.*/
 
-function (first, last) {
+const nameO = (first, last) => {
+    let name1 = first.charAt(0).toUpperCase
+    let name2 = last.charAt(0).toUpperCase
+    console.log(`${name1}, + ${name2}`);
     
 }
+nameO(tommy, kilmo)
