c55c629c3f64fd657db7e1aa8b6d94743e79276f
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-02 20:50:16 -0600

    po

--- funcTsk5.js
+++ funcTsk5.js
@@ -1,2 +1,12 @@
 /*Write a function that calculates and returns an area of a circle.
- The function should have two parameters. One is a default parameter for the pi 3.14. Fix the answer to two decimal places.*/
+ The function should have two parameters. One is a default parameter for the pi 3.14. 
+ Fix the answer to two decimal places.*/
+ 
+ function calcCircArea(radius, pi = 3.14){
+    return (pi * radius * radius).toFixed(2);
+ }
+ console.log(calcCircArea(6));
+ console.log(calcCircArea(12));
+ console.log(calcCircArea(61));
+ console.log(calcCircArea(-6));
+ 
