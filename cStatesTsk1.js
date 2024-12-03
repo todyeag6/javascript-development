1895dfe44a1c183ed70091893db67b8c267542c4
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-03 09:39:31 -0600

    ok

---
+++ cStatesTsk1.js
@@ -0,0 +1,19 @@
+/*Write a function that takes two parameters. Using an if else statement, compare the two parameters
+
+together using the == operator. Use a console log statement for the if block and else block that outputs
+
+whether these two parameters are equal to each other. Test this function using both number and string
+
+values.*/
+
+function lieDetect(param1, param2) {
+    if (param1==param2) {
+        console.log("The two parameters are equal");
+    
+     } else {
+        console.log("The two parameters are not equal");
+        
+    }
+}
+lieDetect(456, 456)// equal
+lieDetect("345, 345")/////  nothing to compare to?
