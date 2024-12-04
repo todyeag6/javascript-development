e216b31b6c6c6fede2450d8fc62743bfb1f63deb
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-04 13:52:52 -0600

    tsk11

--- cStatesTsk11.js
+++ cStatesTsk11.js
@@ -7,3 +7,25 @@ Month 2 is 29 days. Month 2 in a leap year is 28 days.
 Months 4, 6, 9, and 11 have 30 days.
 
 The rest is 31 days.*/
+
+const manyDays=(month, year) => {
+    if(month < 1 || month > 12){
+        return;
+    }
+    if(month == 2) {
+        if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+            return 29;
+          } else {
+            return 28;
+          }
+        }else if (month == 4 || month==6 || month==9 || month==11) {
+            console.log("month has 30 days");
+        }else{
+            console.log("month has 31 days.");
+            
+        }
+            
+}
+console.log(manyDays(4,  2011));
+
+
