955f2304d770dc4f3b9e8831faeea96dbe2b0149
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-11 14:34:44 -0600

    ok

--- class&Object.js
+++ class&Object.js
@@ -41,9 +41,87 @@ const happy = {
 //console.log(happy);
 
 // loop an object
-for (property in myHouse) {
- console.log(`The key name is: ${property}, and the value: ${myHouse[property]}`);
+for (property in myHouse) {}
+ ////console.log(`The key name is: ${property}, and the value: ${myHouse[property]}`);
+//
+
+/////
+/*
+if (Object.hasOwn(myHouse, property)) {
+    console.log(myHouse[property]);
+    
 }
 
+const VolumeCalculater = {
+    findCubeVol:function (side) {
+        return side * 3
+    },
+    findCylindeVol: (cylinderRadius, cylinderHeight) => {
+        return 3.14 * cylinderRadius **2 * cylinderHeight;
+    }
+};
+console.log(VolumeCalculater.findCylindeVol(20,50));*/
+/////objects can have mixture of properties
+const animal = {
+    type: "mammal",
+    sound: () => console.log("roaor"),
+    skin: "fur",
+    gender: "male",
+    arrayKey: [],
+    objkey: {}
 
+};
+//animal.sound()
+//console.log(animal.type);
+
+class Person {
+    #hairColor
+    #skinColor
+    constructor(firstName, lastName, eyeColor, hairColor, skinColor, footSize) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eyeColor = eyeColor;
+        this.footSize = footSize;
+        this.#hairColor = hairColor;//can only be accessed with getter ssetters
+        this.#skinColor =skinColor;  //can only be accessed with getter ssetters      
+        }
+        static staticProperty1= "This property only belongs to the class person itself."
+        static staticMethodProp(){
+            return "this method property only belong to class person"
+        }
+        run(){
+            console.log("the person can run"); 
+              
+            }
+            speak(language) {
+                console.log(this.firstName = " speaks " + language);
+    }
+}
+
+//const curtis = new Person("Curtis", "Axel", "blue", 45, "glows in the dark");
+//const lilly = new Person("Lilly", "Axel", "blue", "brown", "invisible");
+//console.log(Person.staticProperty1);
+//console.log(Person.staticMethodProp());
+
+class City {
+    constructor(population, size, economy, name) {
+        this.population = population;
+        this.size = size;
+        this.economy = economy;
+        this.name = name;
+    }
+    grow(){
+        console.log("It is growing rapidly");
+        
+    }
+}
+class KansaCity extends City {
+    constructor(cityFountains) {
+      super(
+
+      )     
+     this.cityFoountains = cityFountains
+    }}
+    const Kc= new KansaCity(10000, "Fairly good", "Kansas City", 5000);
+    console.log(Kc.cityFoountains);
     
