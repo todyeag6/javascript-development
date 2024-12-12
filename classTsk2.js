6024495287d7c114905dbf97df497259f8f1fb75
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-12 13:14:01 -0600

    ok

--- classTsk2.js
+++ classTsk2.js
@@ -3,10 +3,10 @@
  * Your class should have getter and setter methods for each attribute. Include one method within your person class as a behavior of the class. */
 
 class Person {
-    #firstName
-    #lastName
-    #age
-    #gender
+    #firstName;
+    #lastName;
+    #age;
+    #gender;
 
     constructor(firstName, lastName, age, gender) {
         this.#firstName = firstName;
@@ -15,26 +15,36 @@ class Person {
         this.#gender = gender;
     }
     
-    getAge() {
+    get age() {
         return this.#age;
     }
-    setAge() {
+    set age(age) {
         this.#age = age;
     }
-    getgender() {
-        return this.#gender
+    
+    get gender() {
+        return this.#gender;
     }
-    setLastName() {
-        this.#gender = gender
+    set gender(gender) {
+        this.#gender = gender;
     }
-    introduce() {
-        console.log(
-          `Hello, my name is ${this.#firstName} ${this.#lastName}. I am ${this.#age} years old and ${this.#gender}.`
-        );
+
+    get fullName() {
+        return `${this.#firstName} ${this.#lastName}`;
+    }
+
+    get firstName() {
+        return this.#firstName;
+    }
+    get lastName() {
+        return this.#lastName;
     }
 }
 
-const person = new Person('Jane', 'Doe', 28, 'female');
-person.introduce(); 
-const person1 = new Person('Tom', 'Clancy', 87, 'wierd');
-person1.introduce(); 
+const person = new Person('Jane', 'Doe', 28, 'female'); 
+const person1 = new Person('Tom', 'Clancy', 87, 'weird'); 
+
+console.log(person.fullName); // Jane Doe
+console.log(person1.firstName); // Tom
+console.log(person1.lastName); // Clancy
+
