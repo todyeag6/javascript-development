b66df605e7ff747e77b137470d8af45c2a93741d
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-17 16:15:20 -0600

    ok

---
+++ IntroToWeb/script-js.js
@@ -0,0 +1,7 @@
+// const elementTexts = document.getElementById("first").innerText;
+document.getElementById("first").innerText =
+  "This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph.";
+
+const pElement = document.getElementById("first");
+pElement.style.color = "red";
+pElement.style.fontSize = "25px";
