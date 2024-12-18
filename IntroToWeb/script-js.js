240173961ae7474048f9419f90d3caf8375a7e8e
todyeag6@gmail.com <todyeag6@gmail.com>
2024-12-18 15:14:54 -0600

    ok

--- IntroToWeb/script-js.js
+++ IntroToWeb/script-js.js
@@ -1,7 +1,22 @@
 // const elementTexts = document.getElementById("first").innerText;
-document.getElementById("first").innerText =
+//document.getElementById("first").innerText =
   "This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph.";
 
 const pElement = document.getElementById("first");
 pElement.style.color = "red";
 pElement.style.fontSize = "25px";
+
+const element = document.getElementsByClassName("cool-class");//return object that is for html
+console.log(element);
+
+const elementsTwo = document.querySelectorAll(".cool-class")//returns array for javascript
+console.log(elementsTwo);
+
+elementsTwo.forEach((element) => {
+  element.style.color = "orange"
+})
+
+const pElement2=document.querySelectorAll("p");
+console.log(pElement2);
+
+
