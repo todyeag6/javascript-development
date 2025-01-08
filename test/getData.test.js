885d42b5099cafe66faeb59085085c93d3c1dc7e
todyeag6@gmail.com <todyeag6@gmail.com>
2025-01-08 10:39:03 -0600

    deleted

--- test/getData.test.js
+++
@@ -1,7 +0,0 @@
-const { getData } = require(".index.js");
-
-describe('getData function", function() {
-    it ("should return an object constaining 3 bios", ()=>{
-        const data = getData()
-        expext(data)
-    })
